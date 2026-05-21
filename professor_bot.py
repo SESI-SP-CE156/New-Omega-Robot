f77a63c5919839f00cc63b36a3ba032007db0c06
Henrique Bissoli Malaman Alonso <contato.henrique.bissoli@gmail.com>
2026-05-21 11:09:54 -0300

    fix velocidade

--- professor_bot.py
+++ professor_bot.py
@@ -12,8 +12,8 @@ from typing import Tuple, Optional
 SERIAL_PORT = "/dev/ttyACM0"
 BAUD_RATE = 115200
 MAX_PWM = 140
-MIN_PWM = 140
-BASE_SPEED = 140
+MIN_PWM = 100
+BASE_SPEED = 120
 
 # ==========================================
 # CONFIGURAÇÕES DA CÂMERA E GEOMETRIA
